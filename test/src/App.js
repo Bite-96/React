0eea3c0dac52602767999b223f1719922e97eaa1
jongwoo1126 <lucky4527@gmail.com>
2022-09-08 16:56:24 +0900

    2022/09/08

--- test/src/App.js
+++ test/src/App.js
@@ -2,11 +2,23 @@ import Button from './Button';
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("Created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+
+  return <h1>Hello</h1>
+}
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
+  const [showing, setShowing] = useState(false);
+
+  const onClick1 = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onClick2 = () => setShowing((prev) => !prev);
 
   useEffect(() => {
     console.log("I run only once.");
@@ -26,7 +38,9 @@ function App() {
       <h1 className={styles.title}>Welcome Back!!!</h1>
       <h1>{counter}</h1>
       <input value={keyword} onChange={onChange} type="text" placeholder="Search Here..." /><br/>
-      <button onClick={onClick}>Click Me</button>
+      <button onClick={onClick1}>Click Me</button><br/>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button><br/>
       <Button text={"Continue"}/>
     </div>
   );
