9e36b9360150e44fc1a8376adbff1e9ce0bc20d3
jongwoo1126 <lucky4527@gmail.com>
2022-09-26 22:01:35 +0900

    2022/09/26

--- movie/src/components/Movie.js
+++ movie/src/components/Movie.js
@@ -1,10 +1,16 @@
 import React from "react";
 import propTypes from "prop-types";
-function Movie({ coverImg, title, summary, genres }) {
+import { Link } from "react-router-dom";
+
+function Movie({ id, coverImg, title, summary, genres }) {
     return (
         <div>
             <img src={coverImg} alt="title"/>
-            <h2>{title}</h2>
+            <h2>
+                <Link to={`/movie/${id}`}>
+                    {title}
+                </Link>
+            </h2>
             <p>{summary}</p>
             <ul>
             {genres.map((g) => (
@@ -17,6 +23,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+    id : propTypes.number.isRequired,
     coverImg : propTypes.string.isRequired,
     title : propTypes.string.isRequired,
     summary : propTypes.string.isRequired,
--- movie/src/routes/Home.js
+++ movie/src/routes/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
 function Home() {
@@ -27,6 +27,7 @@ function Home() {
             {movies.map(movie => (
               <Movie 
                 key={movie.id}
+                id={movie.id}
                 coverImg={movie.medium_cover_image} 
                 title={movie.title} 
                 summary={movie.summary} 
