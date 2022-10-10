cab1403a6e56654d0e9a841133ccfd241779947d
jongwoo1126 <lucky4527@gmail.com>
2022-10-10 11:29:00 +0900

    Updates

---
+++ precache-manifest.1e1d34ae05c5a93d2488b8535dfe8ac0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9283686e57152356530d",
+    "url": "/React/static/js/main.9283686e.chunk.js"
+  },
+  {
+    "revision": "1d0e6c261db5125cfca6",
+    "url": "/React/static/js/1.1d0e6c26.chunk.js"
+  },
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "da0bac1fb6ded116b4c9cb732a687f80",
+    "url": "/React/index.html"
+  }
+];
