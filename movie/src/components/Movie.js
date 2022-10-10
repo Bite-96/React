528b2e57a3e4bdb87bc6b7e1679ac07f70e21c97
jongwoo1126 <lucky4527@gmail.com>
2022-10-10 11:41:24 +0900

    2022/10/10

--- movie/src/components/Movie.js
+++ movie/src/components/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import propTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Detail from "../routes/Detail";
 
 function Movie({ id, coverImg, title, summary, genres }) {
     return (
